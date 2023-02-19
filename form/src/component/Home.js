96b5d8668038f0cc2989f6a76b969daef8083ee0
innosufiyan <innosufiyan@gmail.com>
2023-02-19 12:12:29 +0500

    implemented navigation on auth

--- form/src/component/Home.js
+++ form/src/component/Home.js
@@ -5,10 +5,10 @@ import axios from "axios";
 
 function Home() {
   const location = useLocation();
-  const state = location.state.token;
-  console.log(state);
+  const token = location.state.token;
+  console.log(token);
   const api = `http://localhost:4000/user/getUser`;
-  axios.get(api, { headers: { "x-access-token": `${state}` } })
+  axios.get(api, { headers: { "x-access-token": `${token}` } })
     .then(res => {
       console.log("Get User: ", res.data);
       this.setState({
@@ -31,7 +31,7 @@ function Home() {
         <div>
           <Navbar />
           <h3 className="pt-5 text-center">hello,i am Home Page</h3>
-          <h5>{state}</h5>
+          <h5>{token}</h5>
         </div>
       )
     }
